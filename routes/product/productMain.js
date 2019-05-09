840834fe5a0f6902264dfe03a2ecaf73bf61b4e6
Joo <youssefalashmawy24@gmail.com>
2019-05-09 14:53:58 +0200

    Ostor tany yarb

--- routes/product/productMain.js
+++ routes/product/productMain.js
@@ -17,8 +17,9 @@ router.get("/",function(req,res){
    // res.render("workerDashboard")
 });
 
-router.get("/addrequests",function(req,res){
-    
+
+router.get("/addrequests", function(req,res){
+ 
     Product.find({},function(err, products){
         if(err){
             console.log("ERROR");
@@ -28,21 +29,108 @@ router.get("/addrequests",function(req,res){
         }
     });
 
-   // res.render("addrequests")
+   // res.render("addrequests") 5cd2ff1974edd329fcab2d69
  });
 
- router.get("/addrequests/:id",function(req,res){
+    /*
+   // const result = await Product.find({})
+   try{const result = await Product.find({supplier: "5cd2ff1974edd329fcab2d69"}).populate('supplier', 'UserName -_id').select('-_id supplier')
+    console.log(result)
+    res.render('addrequests', {products: result})
+}
+    catch(err){
+        console.log(err.message)
+    }*/
+   
+
+
+ router.get("/pullrequests",function(req,res){
     
-    Product.findBYId(req.params.id,function(err, found_product){
+    Product.find({},function(err, products){
         if(err){
-            res.redirect("addrequests");
+            console.log("ERROR");
         } else {
-                res.render("readMore",{found_product :found_product});
+                res.render("pullrequests",{products :products});
  
         }
     });
 
    // res.render("addrequests")
  });
+
+
+ router.get('/addrequests/readMore',function(req,res){
+    Product.find({},function(err, found_product){
+        if(err){
+            console.log("ERROR");
+        } else {
+                res.render("readMore",{found_product :found_product});
+ 
+        }
+    });
+ });
+
+
+ router.get("/addrequests/:id",function(req,res){
+     
+    Product.findById(req.params.id , function(err , found_product){
+        if(err){
+            res.redirect("/products/addrequests");
+        } else {
+                res.render("readMore",{found_product :found_product});
+ 
+        }
+    });
+
+   // res.send("addrequests")
+ });
+
+
+ router.put("/addrequests/:id",function(req,res){
+    Product.findByIdAndUpdate(req.params.id , function(err,updatedProduct){
+        if(err){
+            res.redirect("/products/addrequests");
+        } else {
+            res.render("readMore",{found_product :updatedProduct});
+        }
+    });
+});
+
+router.put("/accept/:id",function(req,res){
+    Product.findByIdAndUpdate(req.params.id, {accepted:true} ,function(err,products){
+        if(err){
+            console.log(err)
+        }else{
+            res.redirect("/products");
+            // res.render("addrequests",{products :products});
+        }
+    });
+
+});
+
+router.put("/decline/:id",function(req,res){
+    Product.findByIdAndUpdate(req.params.id, {declined:true} ,function(err,products){
+        if(err){
+            console.log(err)
+        }else{
+            res.redirect("/products");
+            // res.render("addrequests",{products :products});
+        }
+    });
+
+});
+
+
+
+router.get('/test', (req, res) => {
+    Product.create({
+        name: 'laptop',
+        price: 20000,
+        supplier: "5cd2ff1974edd329fcab2d69",
+        accepted: false
+    })
+    res.send('Created ya esraa')
+})
+
 
 module.exports = router
